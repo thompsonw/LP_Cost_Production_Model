22a8e5ab3656c3aefc36c58b55f784a175295ca6
thompsonw <thompsonw@carleton.edu>
2020-03-01 20:01:49 -0600

    Fixed indent error

--- find_skipping_coeff.py
+++ find_skipping_coeff.py
@@ -145,21 +145,23 @@ def display_simulation_results(optimal_result):
 
 
 def main():
+    
     random.seed(0)
     csv_input = BaseLoopInputData('Input_Data.csv')
 
-    demand_schedule = csv_input.entire_Demand_Schedule
-    unit_production_time = csv_input.all_Production_Times
-    holding_cost = csv_input.inventory_Cost
+    demand_schedule = csv_input.entire_demand_schedule
+    unit_production_time = csv_input.all_production_times
+    holding_cost = csv_input.inventory_cost
     num_items = len(holding_cost)
     num_periods = len(demand_schedule)
     demand_schedule_init = demand_schedule.copy()
     demand_schedule_init.insert([0]*num_items, 0)
-    changeover_cost = csv_input.changeover_Cost
+    changeover_cost = csv_input.changeover_cost
     initial_inventory = csv_input.initial_inventories
     total_time = csv_input.total_time
     cost_tolerance = csv_input.cost_tolerance
-'''
+
+    '''
     num_items = 3 # total number of items
     num_periods = 11 # total number of time periods
     unit_production_time = [3, 4, 5] # vector of item production time per unit
@@ -181,13 +183,15 @@ def main():
                             [140, 90, 100], [120, 110, 110], [130, 110, 90], \
                             [120, 110, 90], [140, 100, 80], [150, 100, 90], \
                             [140, 80, 120], [140, 90, 110], [130, 110, 100]]
-'''
+    '''
+
     kwargs = {'num_items': num_items, 'num_periods': num_periods, \
               'unit_production_time': unit_production_time, \
               'total_time': total_time, 'initial_inventory': initial_inventory, \
               'demand_schedule': demand_schedule, 'cost_tolerance': cost_tolerance, \
               'changeover_cost': changeover_cost, 'holding_cost': holding_cost, \
               'demand_schedule_init': demand_schedule_init}
+
     optimal_lambdas = cost_model(**kwargs)
 
     # output of skipping model after 1M simulations: [18, 8, 11]
